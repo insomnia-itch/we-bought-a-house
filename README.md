3d0dc32983ac28e3a5f813ba7fbfc179210268cb
Jelani Woods <jelani@firstdraft.com>
2024-07-21 23:19:57 -0500

    Migrate users

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # My app
 
 ![Domain Model](erd.png?raw=true "Domain Model")
+
+
+```
+rails generate devise user name:string
+rails generate scaffold house name:string address:string lister:belongs_to
+rails generate scaffold project priority:integer desired_completion_date:date running_cost:integer house:belongs_to budget:integer estimated_completion_date:date
+rails generate scaffold ownership owner:belongs_to house:belongs_to
+rails generate scaffold contractor name:string phone_number:string email:string
+rails generate scaffold specialty name:string
+rails generate scaffold skill contactor:belongs_to specialty:belongs_to
+rails generate scaffold contract contractor:belongs_to project:belongs_to time_estimate:integer price_estimate:integer running_cost:integer accepted:boolean
+
+rails generate scaffold note notable:references{polymorphic} body:text author:belongs_to
+
+rails generate scaffold receipt project:belongs_to price:string contractor:belongs_to type_of_cost:string
+
+rails generate scaffold prerequisite required_project:belongs_to locked_project:belongs_to
+rails generate scaffold attachment file_extension:string url:string uploader:belongs_to attachable:references{polymorphic}
+
+```
--- db/schema.rb
+++ db/schema.rb
@@ -10,102 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_22_033245) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_22_041740) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
-  create_table "contractors", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.string "phone_number", default: "", null: false
-    t.string "email", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "contracts", force: :cascade do |t|
-    t.bigint "contractor_id", null: false
-    t.bigint "project_id", null: false
-    t.integer "time_estimate", null: false
-    t.integer "price_estimate", null: false
-    t.integer "priority", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["contractor_id"], name: "index_contracts_on_contractor_id"
-    t.index ["project_id"], name: "index_contracts_on_project_id"
-  end
-
-  create_table "houses", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "notes", force: :cascade do |t|
-    t.string "notable_type", null: false
-    t.bigint "notable_id", null: false
-    t.text "body", default: "", null: false
-    t.bigint "author_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_notes_on_author_id"
-    t.index ["notable_type", "notable_id"], name: "index_notes_on_notable"
-  end
-
-  create_table "ownerships", force: :cascade do |t|
-    t.bigint "owner_id", null: false
-    t.bigint "house_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["house_id"], name: "index_ownerships_on_house_id"
-    t.index ["owner_id"], name: "index_ownerships_on_owner_id"
-  end
-
-  create_table "projects", force: :cascade do |t|
-    t.string "style", default: "", null: false
-    t.integer "priority", default: 0, null: false
-    t.integer "time_to_complete", default: 0, null: false
-    t.integer "price", default: 0, null: false
-    t.bigint "house_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["house_id"], name: "index_projects_on_house_id"
-  end
-
-  create_table "skills", force: :cascade do |t|
-    t.bigint "contractor_id", null: false
-    t.bigint "specialty_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["contractor_id"], name: "index_skills_on_contractor_id"
-    t.index ["specialty_id"], name: "index_skills_on_specialty_id"
-  end
-
-  create_table "specialties", force: :cascade do |t|
-    t.string "name", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
+    t.string "name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "contracts", "contractors"
-  add_foreign_key "contracts", "projects"
-  add_foreign_key "notes", "users", column: "author_id"
-  add_foreign_key "ownerships", "houses"
-  add_foreign_key "ownerships", "users", column: "owner_id"
-  add_foreign_key "projects", "houses"
-  add_foreign_key "skills", "contractors"
-  add_foreign_key "skills", "specialties"
 end
